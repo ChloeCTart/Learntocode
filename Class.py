2ed0b84d93a78ecb3b68e0341b158bb6919efbeb
ChloeCTart <chau.dn23p0498@gmail.com>
2025-11-17 21:46:04 +0700

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,33 @@
+class Student:
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+    def introduce(self):
+        print('Em ten la',self.name,'.Em nam nay',self.age)
+
+    def calculate_person_average_score(self):
+        s=sum(self.score.values())
+        print('Diem trung binh cua',self.name,'la',s/len(self.score))
+
+    def calculate_class_average_score(students):
+        if not students:
+            print('Loi')
+        s = 0
+        t=0
+        for i in students:
+            s=sum(i.score.values())
+            a=s/len(i.score)
+            t=t+a
+        print('Diem trung binh cua lop la',t/len(students))
+
+s1 = Student('An', 18, {'Toan': 9, 'Anh': 8, 'Van': 7})
+s2 = Student("Binh", 17, {'Toan': 6, 'Anh': 7, 'Van': 8})
+s3 = Student("Cuong", 16, {'Toan': 10, 'Anh': 9, 'Vam': 9})
+
+students = [s1, s2, s3]
+
+s1.introduce()
+Student.calculate_person_average_score(s1)
+Student.calculate_class_average_score(students)
---
+++ Function1.py
@@ -0,0 +1,7 @@
+def chan_le(a):
+    if a%2==0:
+        print(a,'la so chan')
+    else:
+        print(a,'la so le')
+a=int(input('Nhap so: '))
+chan_le(a)
